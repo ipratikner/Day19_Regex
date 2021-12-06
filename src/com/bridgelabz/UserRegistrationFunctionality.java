92c8e2ce674998f0f74d69eef07629879aeed35a
ipratikner <pratiknerlikar@gmail.com>
2021-12-06 22:52:31 +0530

    Added: code to validate first name of user

---
+++ src/com/bridgelabz/UserRegistrationFunctionality.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class UserRegistrationFunctionality {
+    Scanner scanner = new Scanner(System.in);
+    UserRegFormValidation userDetails = new UserRegFormValidation();
+
+    public void userFirstName() {
+        System.out.println("Enter Your First Name : ");
+        String firstName = scanner.next();
+        userDetails.firstName(firstName);
+    }
+}
